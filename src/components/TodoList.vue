79c4b5cd6cd23473514ef26924107d98099f3732
“rskidmore1” <ryan.skidmore@yahoo.com>
2022-09-06 11:31:55 -0700

    Adding methods to main.js.

---
+++ src/components/TodoList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="todo-list">
+    <div class="list-item-holder" v-if="n.location == location" :data-status="n.completed">
+      <input type="checkbox" :data-id="n.id" :id="n.id" @click="updateTodo" :checked="n.completed" /><label :data-id="n.id" :for="n.id"> </label>
+      <div class="delete-item" @click="deleteItem" :data-id="n.id">Delete</div>
+      <div class="archive-item" v-if="n.location !== 'archive'" @click="archiveItem" :data-id="n.id">Archive</div>
+    </div>
+  </div>
+  <div id="new-todo-list-item">
+    <input type="text" id="new-todo-list-item-input" @keyup="updateItemText" />
+    <input type="submit" id="new-todo-list-item-submit" @click="newItem" value="Add To Do List Item" />
+  </div>
+</template>
+
+<script>
+    import { useStore } from 'vuex';
+    import { v4 as uuidv4 } from 'uuid';
+
+    // export default {
+    //   beforeCreate(){
+    //     // get our store
+    //     const store = useStore();
+    //     store.commit('loadStore')
+    //   }
+    // }
+
+    export default {
+      name: "TodoList",
+      data(){
+        return {
+          newTodoItem: ''
+        }
+      },
+      props: { location: String },
+      setup() {
+        const store = useStore();
+
+        return {
+          todos: store.getters.todos
+        }
+      }
+    }
+
+  </script>
--- src/main.js
+++ src/main.js
@@ -26,6 +26,15 @@ const store = createStore({
 
   },
   mutations: {
+    loadStore(){
+      if(localStorage.getItem('store')){
+        try {
+          this.replaceState(JSON.parse(localStorage.getItem('store')));
+        } catch(e){
+           console.log('Could not initialize store', e);
+        }
+      }
+    }
     updateTodo(state, todoItem) {
 
       let id = todoItem.id;
@@ -43,8 +52,76 @@ const store = createStore({
       } else {
         console.log(`To Do List ${id} couldn't be found`)
       }
+    },
+    addTodo(state, todoItem){
+      if(todoItem.id !== undefined && typeof todoItem.name == 'string' && typeof todoItem.completed == 'boolean'){
+        state.todos.push({
+          id: todoItem.id,
+          name: todoItem.name,
+          completed: todoItem.completed,
+          location: 'home'
+        })
+      },
+      deleteTodo(state, todoItem){
+        let id = todoItem.id;
+        let removedEl = state.todos.findIndex((x)=> x.id == id);
+        if(removedEl !== null){
+          state.todos.splice(removedEl, 1);
+        }
+      },
+      moveTodoItem(state, todoItem){
+        let id = todoItem.id;
+        let location = todoItem.location;
+        let findEl = state.todos.find((x) => x.id == id);
+
+        if(findEl !== null){
+          findEl.location = location;
+        } else {
+             console.log(`To Do List Item ${id} couldn't be found`);
+        }
+      }
+    }
+  },
+  methods: {
+    updateItemText: function(e){
+      this.newTodoItem = e.currentTarget.value;
+      if(e.keyCode === 13){
+        this.newTodoItem();
+      }
+      return false;
+    },
+    updateTodo: function(e){
+      let newStatus = e.currentTarget.parentElement.getAttribute('data-status') == "true" ? false : true;
+      this.$store.commit('updateTodo', {
+        id: e.currentTarget.getAttribute('data-id'),
+        completed: newStatus
+      })
+    },
+    deleteItem: function(e){
+      this.$store.commit('deleteTodo', {
+        id: e.currentTarget.getAttribute('data-id')
+      })
+    },
+    newItem: function(){
+      if(this.newTodoItem !== ''){
+        this.$store.commit('addTodo', {
+          id: uuidv4(),
+          name: this.newTodoItem,
+          completed: false
+        })
+      }
+    },
+    archiveItem: function(e){
+      this.$store.commit('moveTodoItem', {
+        id: e.currentTarget.getAttribute('data-id'),
+        location: 'archive'
+      })
     }
   }
+})
+
+store.subscribe((mutation, state) => {
+  localStorage.setItem('store', JSON.stringify(state))
 })
 
 app.use(router).use(store).mount('#app')
